2bc7da2ef35f641b52c2418f62e7c7a0d477544a
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-21 14:14:00 +0300

    Example 012 Methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,9 @@
+﻿//Использование цикла в цикле
+for(int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 9; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
